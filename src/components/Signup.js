21b827e25564ba53a22ad0e7c0010075f287b1a5
Yash Jangir <yashjangirrr@gmail.com>
2022-02-04 20:46:59 +0530

    minor changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -152,18 +152,6 @@ const Signup = (props) => {
                 >
                   Sign Up
                 </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link href="/" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
               </Box>
             </Box>
           </Grid>
